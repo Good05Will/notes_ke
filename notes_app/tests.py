d83baf658873ad44dec96d28c66cec203ddf32a2
123 <123@12.com>
2024-09-10 14:56:03 +0500

    add NotesAPITestCase

--- notes_app/tests.py
+++ notes_app/tests.py
@@ -1,14 +1,16 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from rest_framework import status
+from django.contrib.auth import logout
 from .models import Note
 
 
-class TestNotesСase(TestCase):
-    """Класс тестирования Note"""
+class NotesTestСase(TestCase):
+    """Класс тестирования представлений notes_app"""
 
     def setUp(self):
-        """Настройки теста. Создание тестового пользователя и заметки"""
+        """Настройки теста. Создание тестового пользователя и заметок"""
         self.user = User.objects.create_user(username='testuser',
                                              password='testpassword')
         self.note1 = Note.objects.create(user=self.user,
@@ -47,15 +49,47 @@ class TestNotesСase(TestCase):
         response = self.client.get('/edit_note/1/')
         self.assertEqual(response.status_code, 200)
 
-    def test_api_get(self):
-        """Тестирование API для получения всех заметок"""
-        response = self.client.get('/api/v1/notes/')
-        self.assertEqual(response.status_code, 200)
 
-    def test_api_post(self):
-        """Тестирование API создания новой заметки"""
+class NotesAPITestCase(APITestCase):
+    """Класс тестирования API notes_app"""
+
+    def setUp(self):
+        """Настройки теста. Создание тестового пользователя и заметок"""
+        self.user = User.objects.create_user(username='testuser',
+                                             password='testpassword')
+        self.note1 = Note.objects.create(user=self.user,
+                                         title='test title 1',
+                                         text='test text 1',
+                                         id=1)
+        self.note2 = Note.objects.create(user=self.user,
+                                         title='test title 2',
+                                         text='test text 2',
+                                         id=2)
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_get_notes(self):
+        """Тестирование получения всех заметок"""
+        response = self.client.get('/api/v1/notes/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['notes']), 2)
+
+    def test_create_note(self):
+        """Тестирование создания новой заметки"""
         data = {'title': 'New Note', 'text': 'New text'}
         response = self.client.post('/api/v1/notes/', data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['note']['title'], data['title'])
         self.assertEqual(response.data['note']['text'], data['text'])
+
+    def test_create_note_missing_fields(self):
+        """Тестирование создания заметки с некорректными данными"""
+        data = {'title': 'New Note'}
+        response = self.client.post('/api/v1/notes/', data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unauthorized_access(self):
+        """Тестирование доступа без авторизации."""
+        logout(self.client)
+        response = self.client.get('/api/v1/notes/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
